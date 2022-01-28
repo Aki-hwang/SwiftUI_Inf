2629f1f3fcd0c1bda4ade99101edc50e76f7608a
chuljin Hwang <chljin.hwang@gmail.com>
2022-01-28 22:58:32 +0900

    Inf_Basic_End

---
+++ Inf_Basic/MygridApp.swift
@@ -0,0 +1,20 @@
+//
+//  MygridApp.swift
+//  Inf_Basic
+//
+//  Created by Chuljin Hwang on 2022/01/28.
+//
+
+import SwiftUI
+
+struct MygridApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MygridApp_Previews: PreviewProvider {
+    static var previews: some View {
+        MygridApp()
+    }
+}
